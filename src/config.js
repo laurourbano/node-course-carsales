207cb9144cd62aa7af51282b7e79d87b45acdaf9
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-11-03 06:34:44 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
-  connectionUrlDatabase: 'mongodb+srv://lauro:@Lauro2007?@carsalesdb.ygv7zcf.mongodb.net/?retryWrites=true&w=majority',
-  apiPort: 8090,
+  connectionUrlDatabase: 'mongodb+srv://lauro:lauro2007@carsalesdb.ygv7zcf.mongodb.net/?retryWrites=true&w=majority',
+  apiPort: 8080,
   corsOptions: {
     origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
   },
